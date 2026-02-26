77d42e0f1597be262d8f2fd6d4327bff879711fa
Kau7almeida <kau7almeida.06@gmail.com>
2026-02-25 21:53:04 -0300

    inicialização do server

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("teste");
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+    logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler(request, reply) {
+    const pessoas = [
+        { nome: "kauã", idade: 19 },
+        { nome: "gabriel", idade: 27 },
+    ];
+    return pessoas;
+})
+
+// Run the server!
+try {
+    await fastify.listen({ port: 3000 })
+} catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+}
