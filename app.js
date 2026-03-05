6ef86a4f34b2618dbdf8b03de92ea8bf3a1919f0
Kau7almeida <kau7almeida.06@gmail.com>
2026-03-04 21:44:44 -0300

    código atualizado aula 2 de backend

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
-// Import the framework and instantiate it
 import Fastify from 'fastify'
 
 const fastify = Fastify({
     logger: true
 })
 
-// Declare a route
-fastify.get('/', async function handler(request, reply) {
-    const pessoas = [
-        { nome: "kauã", idade: 19 },
-        { nome: "gabriel", idade: 27 },
-    ];
-    return pessoas;
+fastify.get('/', () => {
+    return "server is running at port 3000";
 })
 
-// Run the server!
+let users = [];
+
+// rota de busca
+fastify.get("/users", async () => {
+    return users;
+});
+
+// criar usuarios
+fastify.post("/users", async (req, reply) => {
+    // captura o corpo da requisição
+    let obj = await req.body;
+    // insere o nome enviado na request para a lista de usuários
+    users.push(obj.nome);
+    // retorna uma mensagem ao usuário
+    return `O usuário ${obj.nome} foi adicionado`;
+});
+
+// atualizar usuarios
+fastify.put("/users", async (req, reply) => {
+    let obj = await req.body;
+    users.splice(obj.indice, 1, obj.nome);
+    return `O usuário foi atualizado para ${obj.nome}`;
+});
+
+// deletar usuarios
+fastify.delete("/users", async (req, reply) => {
+    let obj = await req.body;
+    users.splice(obj.indice, 1);
+    return "Usuário deletado com sucesso"
+});
+
 try {
     await fastify.listen({ port: 3000 })
 } catch (err) {
